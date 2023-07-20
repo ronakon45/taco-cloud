e562490b715eed6a7f58e7fdfce3fe118362dbe5
Ronakon Watt <88375612+ronakon45@users.noreply.github.com>
2023-07-20 10:40:53 +0700

    Update JdbcIngredientRepository.java

--- src/main/java/tacos/data/JdbcIngredientRepository.java
+++ src/main/java/tacos/data/JdbcIngredientRepository.java
@@ -14,12 +14,13 @@ import tacos.Ingredient;
 
 @Repository
 public class JdbcIngredientRepository implements IngredientRepository {
- 
-  private JdbcTemplate jdbcTemplate;
- 
-  public JdbcIngredientRepository(JdbcTemplate jdbcTemplate) {
-    this.jdbcTemplate = jdbcTemplate;
-  }
+
+	private JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	public JdbcIngredientRepository(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 
 	@Override
 	public Iterable<Ingredient> findAll() {
@@ -27,21 +28,14 @@ public class JdbcIngredientRepository implements IngredientRepository {
 	}
 
 	public Optional<Ingredient> findById(String id) {
-	  List<Ingredient> results = jdbcTemplate.query(
-	      "select id, name, type from Ingredient where id=?",
-	      this::mapRowToIngredient,
-	      id);
-	  return results.size() == 0 ?
-	          Optional.empty() :
-	          Optional.of(results.get(0));
+		List<Ingredient> results = jdbcTemplate.query("select id, name, type from Ingredient where id=?",
+				this::mapRowToIngredient, id);
+		return results.size() == 0 ? Optional.empty() : Optional.of(results.get(0));
 	}
-	
-	private Ingredient mapRowToIngredient(ResultSet row, int rowNum)
-	    throws SQLException {
-	  return new Ingredient(
-	      row.getString("id"),
-	      row.getString("name"),
-	      Ingredient.Type.valueOf(row.getString("type")));
+
+	private Ingredient mapRowToIngredient(ResultSet row, int rowNum) throws SQLException {
+		return new Ingredient(row.getString("id"), row.getString("name"),
+				Ingredient.Type.valueOf(row.getString("type")));
 	}
 
 	@Override
