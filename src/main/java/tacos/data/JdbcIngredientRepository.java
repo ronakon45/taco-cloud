28f18ea1545b59ec829ec390f873e86a6314098c
Ronakon Watt <88375612+ronakon45@users.noreply.github.com>
2023-07-20 11:45:25 +0700

    Update JdbcIngredientRepository.java

--- src/main/java/tacos/data/JdbcIngredientRepository.java
+++ src/main/java/tacos/data/JdbcIngredientRepository.java
@@ -48,8 +48,11 @@ public class JdbcIngredientRepository implements IngredientRepository {
 
 	@Override
 	public Ingredient save(Ingredient ingredient) {
-		jdbcTemplate.update("insert into Ingredient (id, name, type) values (?, ?, ?)", ingredient.getId(),
-				ingredient.getName(), ingredient.getType().toString());
-		return ingredient;
+	  jdbcTemplate.update(
+	      "insert into Ingredient (id, name, type) values (?, ?, ?)",
+	      ingredient.getId(),
+	      ingredient.getName(),
+	      ingredient.getType().toString());
+	  return ingredient;
 	}
 }
